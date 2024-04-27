573e470ffa539924a8f332cf49b84593ce9caddf
campbell-frost <campbellsfrost@gmail.com>
2024-04-27 19:40:13 -0400

    added buttons that change shapes

--- src/components/Visualize.vue
+++ src/components/Visualize.vue
@@ -1,11 +1,17 @@
 <template>
   <v-container class="container">
-    <v-row class="mt-5"> </v-row>
+    <v-row class="mt-5">
+    </v-row>
     <v-row>
       <v-col>
         <div ref="canvas" class="canvas" />
       </v-col>
       <v-col>
+        <div class="d-flex justify-space-between mb-6 ">
+          <v-btn class="flex-grow-1 mx-2" variant="outlined" @click="toggleShape('cube')">Cube</v-btn>
+          <v-btn class="flex-grow-1 mx-2" variant="outlined" @click="toggleShape('plane')">Plane</v-btn>
+          <v-btn class="flex-grow-1 mx-2" variant="outlined" @click="toggleShape('sphere')">Sphere</v-btn>
+        </div>
         <v-slider v-model="zPos" label="Z position" :step="1" max="100" min="2" track-color="grey" />
       </v-col>
     </v-row>
@@ -21,18 +27,20 @@ export default {
   setup() {
     const canvas = ref(null);
     const zPos = ref(1);
+    const currentShape = ref('plane');
+    let scene, camera, renderer;
 
     const createScene = () => {
-      const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-      const renderer = new THREE.WebGLRenderer();
+      scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+      renderer = new THREE.WebGLRenderer();
       renderer.setSize(500, 500);
       canvas.value?.appendChild(renderer.domElement);
       return { scene, camera, renderer };
     };
 
     const createCube = () => {
-      const geometry = new THREE.BoxGeometry();
+      const geometry = new THREE.BoxGeometry(10, 10, 10);
       const material = new THREE.MeshBasicMaterial({ color: '#ffffff' });
       return new THREE.Mesh(geometry, material);
     };
@@ -49,32 +57,42 @@ export default {
       return new THREE.Mesh(geometry, material);
     };
 
+    const toggleShape = (shape) => {
+      currentShape.value = shape;
+      scene.clear();
+      const shapeToAdd =
+        shape === 'cube'
+          ? createCube()
+          : shape === 'plane'
+            ? createPlane()
+            : createSphere();
+      scene.add(shapeToAdd);
+    };
+
+    // Runs when the page loads
     onMounted(() => {
       const { scene, camera, renderer } = createScene();
-
-      const cube = createCube();
-      const plane = createPlane();
-      const sphere = createSphere();
-
-      scene.add(plane);
+      toggleShape('plane');
 
       // Animate function
-      const animate = function (shape) {
-        requestAnimationFrame(() => animate(shape));
-        shape.rotation.x += 0.01;
-        shape.rotation.y += 0.01;
+      const animate = function () {
+        requestAnimationFrame(animate);
+        scene.children.forEach((child) => {
+          child.rotation.x += 0.01;
+          child.rotation.y += 0.01;
+        });
         renderer.render(scene, camera);
       };
 
-      animate(plane);
+      animate();
 
-      // Watch for changes in zPos and update camera position accordingly
+      // Watches for changes in zPos and update camera position accordingly
       watch(zPos, (newValue) => {
         camera.position.z = parseFloat(newValue);
       });
     });
 
-    return { canvas, zPos };
+    return { canvas, zPos, toggleShape };
   }
 };
 </script>
